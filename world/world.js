2aba94300f1070317fb400819f62c23931ef11ed
Zack P <i-need-fire@hotmail.com>
2019-10-06 12:42:16 -0500

    removed unused functionality from world

--- world/world.js
+++ world/world.js
@@ -1,13 +1,8 @@
 const Instance = require('./instance')
-const IndexOrganizer = require('./index-organizer')
-const Player = require('../entity/player')
 
 class World extends Instance {
     constructor({ maximumPlayers, maximumNpcs, maximumObjects }) {
         super(maximumPlayers, maximumNpcs, maximumObjects)
-        this.playerIndex = new IndexOrganizer()
-        this.npcIndex = new IndexOrganizer()
-        this.objectIndex = new IndexOrganizer()
     }
 }
 
